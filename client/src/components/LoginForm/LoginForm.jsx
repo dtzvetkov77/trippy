56deada46561639767790763c2786c0ed22891e3
Daniel Tsvetkov <zvetkovd@gmail.com>
2023-06-05 12:22:53 +0300

    finish authorization process

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,47 +1,141 @@
 import { useNavigate, Link } from 'react-router-dom';
 import './LoginForm.css'
-import axios from "axios";
 import { useState } from 'react';
 
 const LoginForm = () => {
     const navigate = useNavigate();
 
 
-  const [user, setUser] = useState({
+  const [formData, setFormData] = useState({
     username: '',
     password: '',
   })
-  const changeHandler = (e) => {
-      setUser((state) => ({
-        ...state,
-        [e.target.name]: e.target.value
-      }))
-  }
+  const [errors, setErrors] = useState({});
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
 
-  const submitHandler = async (e) => {
-      e.preventDefault();
-     axios.post('/api/auth/login', user)
-      navigate('/')
-  }
+    // Validate the field as the user types
+    const formErrors = validateField(name, value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: formErrors[name],
+    }));
+  };
+
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    const formErrors = validateField(name, value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: formErrors[name],
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length === 0) {
+      try {
+        // Send login request
+        const response = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          // User logged in successfully
+          console.log("User logged in!");
+          const data = await response.json();
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("username", formData.username);
+          navigate('/')
+
+          // Reset form data
+          setFormData({
+            email: "",
+            password: "",
+          });
+
+          setErrors({});
+        } else {
+          // Handle error response from the server
+          console.error("Error logging in");
+        }
+      } catch (error) {
+        // Handle any network or server error
+        console.error("Error logging in", error);
+      }
+    } else {
+      setErrors(formErrors);
+    }
+  };
+
+  const validateField = (fieldName, value) => {
+    const fieldErrors = {};
+
+    switch (fieldName) {
+      case "username":
+        if (!value.trim()) {
+          fieldErrors[fieldName] = "Username is required";
+        }
+        break;     
+      case "password":
+        if (!value) {
+          fieldErrors[fieldName] = "Password is required";
+        } else if (value.length < 6) {
+          fieldErrors[fieldName] = "Password must be at least 6 characters long";
+        }
+        break;
+      default:
+        break;
+    }
+
+    return fieldErrors;
+  };
+
+  const validateForm = () => {
+    const formErrors = {};
+
+    for (const fieldName in formData) {
+      const fieldErrors = validateField(fieldName, formData[fieldName]);
+      if (Object.keys(fieldErrors).length > 0) {
+        formErrors[fieldName] = fieldErrors[fieldName];
+      }
+    }
+
+    return formErrors;
+  };
 
   
   return (
     <div className='form-container'>
       <h1>Login</h1>
-      <form onSubmit={submitHandler}>
+
+      <form onSubmit={handleSubmit}>
+      {errors.username && <p className='error'>{errors.username}</p>}
       <input
       type='text'
       name='username'
       placeholder='Username' 
-      value={user.username} 
-      onChange={changeHandler}  />
+      value={formData.username} 
+      onChange={handleChange}
+       onBlur={handleBlur} />
+        {errors.username && <p className='error'>{errors.username}</p>}
       <input 
       type='text'
       placeholder='Password'
-      value={user.password}
+      value={formData.password}
       name='password'
-      onChange={changeHandler}
+      onChange={handleChange}
+      onBlur={handleBlur}
        />
       <button>Login</button>
       <p className='login-link'>You don't have an account?<Link style={{textDecoration: 'none', color: 'black'}} to='/register'><span>Register</span></Link></p>
--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -11,50 +11,153 @@ const RegisterForm = () => {
 
     const { userRegister } = useContext(AuthContext);
 
-  const [user, setUser] = useState({
+  const [formData, setFormData] = useState({
     username: '',
     email:'',
     password: '',
   })
-  const changeHandler = (e) => {
-      setUser((state) => ({
-        ...state,
-        [e.target.name]: e.target.value
-      }))
-  }
+ 
+  
+  const [errors, setErrors] = useState({});
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
 
-  const submitHandler = async (e) => {
-      e.preventDefault();
-     axios.post('/api/auth/register', user)
-     userRegister(user)
-      navigate('/login')
-  }
+    // Validate the field as the user types
+    const formErrors = validateField(name, value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: formErrors[name],
+    }));
+  };
 
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    const formErrors = validateField(name, value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: formErrors[name],
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length === 0) {
+      try {
+        // Save user in MongoDB
+        const response = await fetch("/api/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          // User saved successfully
+          console.log("User registered!");
+
+         
+          // Reset form data
+          setFormData({
+            username: "",
+            email: "",
+            password: "",
+          });
+          navigate('/login')
+          setErrors({});
+        } else {
+          // Handle error response from the server
+          console.error("Error registering user");
+        }
+      } catch (error) {
+        // Handle any network or server error
+        console.error("Error registering user", error);
+      }
+    } else {
+      setErrors(formErrors);
+    }
+  };
+
+  const validateField = (fieldName, value) => {
+    const fieldErrors = {};
+
+    switch (fieldName) {
+      case "username":
+        if (!value.trim()) {
+          fieldErrors[fieldName] = "First name is required";
+        }
+        break;
+      case "email":
+        if (!value.trim()) {
+          fieldErrors[fieldName] = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(value)) {
+          fieldErrors[fieldName] = "Invalid email address";
+        }
+        break;
+      case "password":
+        if (!value) {
+          fieldErrors[fieldName] = "Password is required";
+        } else if (value.length < 6) {
+          fieldErrors[fieldName] = "Password must be at least 6 characters long";
+        }
+        break;
+      default:
+        break;
+    }
+
+    return fieldErrors;
+  };
+
+  const validateForm = () => {
+    const formErrors = {};
+
+    for (const fieldName in formData) {
+      const fieldErrors = validateField(fieldName, formData[fieldName]);
+      if (Object.keys(fieldErrors).length > 0) {
+        formErrors[fieldName] = fieldErrors[fieldName];
+      }
+    }
+
+    return formErrors;
+  };
   
   return (
     <div className='form-container'>
       <h1>Register</h1>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={handleSubmit}> 
+      {errors.username && <p className='error'>{errors.username}</p>}
       <input
       type='text'
       name='username'
       placeholder='Username' 
-      value={user.username} 
-      onChange={changeHandler}  />
+      value={formData.username} 
+      onChange={handleChange}
+      onBlur={handleBlur}  />
+     
+        
+     {errors.email && <p className='error'>{errors.email}</p>}
       <input 
-      type='email'
+      type='text'
       placeholder='Email'
-      value={user.email}
+      value={formData.email}
       name='email'
-      onChange={changeHandler}
+      onChange={handleChange}
+      onBlur={handleBlur}
        />
+       {errors.password && <p className='error'>{errors.password}</p>}
       <input 
       type='text'
       placeholder='Password'
-      value={user.password}
+      value={formData.password}
       name='password'
-      onChange={changeHandler}
+      onChange={handleChange}
+      onBlur={handleBlur}
        />
        
       <button>Register</button>
